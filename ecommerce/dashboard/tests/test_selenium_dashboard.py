9f5c358ff640b38462a20a1be5f2dbfc72b4f84c
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-11 14:39:40 +0800

    Connect fixtures with json file and hashed admin password

--- ecommerce/dashboard/tests/test_selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -1,18 +1,21 @@
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from django.contrib.auth.models import User
 
 
-@pytest.mark.selenium
-def test_create_new_admin_user(create_admin_user):
-    assert create_admin_user.__str__() == "admin"
+# @pytest.mark.selenium
+# def test_create_new_admin_user(create_admin_user):
+#     assert create_admin_user.__str__() == "admin"
 
 
 @pytest.mark.selenium
 def test_dashboard_admin_login(
-    live_server, create_admin_user, chrome_browser_instance
+    live_server, db_fixture_setup, chrome_browser_instance
 ):
     """Test for the login of the admin user."""
+    i = User.objects.get(id=1)
+    print(i.username)
     browser = chrome_browser_instance
 
     browser.get(("%s%s" % (live_server.url, "/admin/login/")))
