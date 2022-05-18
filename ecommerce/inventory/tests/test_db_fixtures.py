146969ff4c519b835d5c116ae413a5bd4be1d147
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-18 18:36:22 +0800

    Add testcases to check manatomany connection between category and product table

--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -1,5 +1,6 @@
 import pytest
 from ecommerce.inventory import models
+from django.db import IntegrityError
 
 
 @pytest.mark.dbfixture
@@ -42,21 +43,39 @@ def test_inventory_db_category_insert_data(
 @pytest.mark.parametrize(
     "id, web_id, name, slug, description, is_active, created_at, updated_at",
     [
-        (11111, 12323232, "Fashion", "fashion", "Fashion", 1, "2020-01-01 00:00:00", "2020-01-01 00:00:00"),
-        (123238, 54687828, "Trainers", "trainers", "Trainers", 1, "2020-01-01 00:00:00", "2020-01-01 00:00:00"),
-        (46876835, 546455468735, "Baseball", "baseball", "Baseball", 1, "2020-01-01 00:00:00", "2020-01-01 00:00:00"),
+        (
+            1,
+            "45425810",
+            "widstar running sneakers",
+            "widstar-running-sneakers",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta, eros vel sollicitudin lacinia, quam metus gravida elit, a elementum nisl neque sit amet orci. Nulla id lorem ac nunc cursus consequat vitae ut orci. In a velit eu justo eleifend tincidunt vel eu turpis. Praesent eu orci egestas, lobortis magna egestas, tincidunt augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean vitae lectus eget tortor laoreet efficitur vel et leo. Maecenas volutpat eget ante id tempor. Etiam posuere ex urna, at aliquet risus tempor eu. Aenean a odio odio. Nunc consectetur lorem ante, interdum ultrices elit consectetur sit amet. Vestibulum rutrum interdum nulla. Cras vel mi a enim eleifend blandit. Curabitur ex dui, rutrum et odio sit amet, auctor euismod massa.",
+            1,
+            "2021-09-04 22:14:18",
+            "2021-09-04 22:14:18",
+        ),
+        (
+            8616,
+            "45434425",
+            "impact puse dance shoe",
+            "impact-puse-dance-shoe",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta, eros vel sollicitudin lacinia, quam metus gravida elit, a elementum nisl neque sit amet orci. Nulla id lorem ac nunc cursus consequat vitae ut orci. In a velit eu justo eleifend tincidunt vel eu turpis. Praesent eu orci egestas, lobortis magna egestas, tincidunt augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean vitae lectus eget tortor laoreet efficitur vel et leo. Maecenas volutpat eget ante id tempor. Etiam posuere ex urna, at aliquet risus tempor eu. Aenean a odio odio. Nunc consectetur lorem ante, interdum ultrices elit consectetur sit amet. Vestibulum rutrum interdum nulla. Cras vel mi a enim eleifend blandit. Curabitur ex dui, rutrum et odio sit amet, auctor euismod massa.",
+            1,
+            "2021-09-04 22:14:18",
+            "2021-09-04 22:14:18",
+        ),
     ],
 )
 def test_inventory_product_dbfixture(
     db,
-    db_fixture_setup,
-    id, web_id,
+    django_db_setup,
+    id,
+    web_id,
     name,
     slug,
     description,
     is_active,
     created_at,
-    updated_at
+    updated_at,
 ):
     result = models.Product.objects.get(id=id)
     result_created_at = result.created_at.strftime("%Y-%m-%d %H:%M:%S")
@@ -69,14 +88,18 @@ def test_inventory_product_dbfixture(
     assert result_created_at == created_at
     assert result_updated_at == updated_at
 
-def test_inventroy_db_product_uniqueness_integrity(db, product_factory):
-    new_web_id = product_factory.create(web_id=12345)
+def test_inventory_db_product_uniqueness_integrity(db, product_factory):
+    new_web_id = product_factory.create(web_id=123456789)
     with pytest.raises(IntegrityError):
-        new_web_id = product_factory.create(web_id=12345)
+        product_factory.create(web_id=123456789)
 
-def test_inventory_db_product_insert_data(db, category_factory, product_factory):
+
+@pytest.mark.dbfixture
+def test_inventory_db_product_insert_data(
+    db, product_factory, category_factory
+):
     new_category = category_factory.create()
-    new_product = product_factory.create(category=(1, 35))
+    new_product = product_factory.create(category=(1, 36))
     result_product_category = new_product.category.all().count()
     assert "web_id_" in new_product.web_id
     assert result_product_category == 2
