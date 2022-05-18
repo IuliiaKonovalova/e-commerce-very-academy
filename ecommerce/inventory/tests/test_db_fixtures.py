f1eeeb50d9367eb0fca51f184ae6508413f62b84
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-18 16:08:49 +0800

    Add test casees for product table

--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -36,3 +36,47 @@ def test_inventory_db_category_insert_data(
 
     assert result.slug == slug
     assert result.is_active == is_active
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, web_id, name, slug, description, is_active, created_at, updated_at",
+    [
+        (11111, 12323232, "Fashion", "fashion", "Fashion", 1, "2020-01-01 00:00:00", "2020-01-01 00:00:00"),
+        (123238, 54687828, "Trainers", "trainers", "Trainers", 1, "2020-01-01 00:00:00", "2020-01-01 00:00:00"),
+        (46876835, 546455468735, "Baseball", "baseball", "Baseball", 1, "2020-01-01 00:00:00", "2020-01-01 00:00:00"),
+    ],
+)
+def test_inventory_product_dbfixture(
+    db,
+    db_fixture_setup,
+    id, web_id,
+    name,
+    slug,
+    description,
+    is_active,
+    created_at,
+    updated_at
+):
+    result = models.Product.objects.get(id=id)
+    result_created_at = result.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    result_updated_at = result.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+    assert result.web_id == web_id
+    assert result.name == name
+    assert result.slug == slug
+    assert result.description == description
+    assert result.is_active == is_active
+    assert result_created_at == created_at
+    assert result_updated_at == updated_at
+
+def test_inventroy_db_product_uniqueness_integrity(db, product_factory):
+    new_web_id = product_factory.create(web_id=12345)
+    with pytest.raises(IntegrityError):
+        new_web_id = product_factory.create(web_id=12345)
+
+def test_inventory_db_product_insert_data(db, category_factory, product_factory):
+    new_category = category_factory.create()
+    new_product = product_factory.create(category=(1, 35))
+    result_product_category = new_product.category.all().count()
+    assert "web_id_" in new_product.web_id
+    assert result_product_category == 2
